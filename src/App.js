8ef4276820e51b9c0dc554afdf8f487f21800c60
Jingyu Cao <jingyu_cao@hotmail.com>
2021-12-29 22:04:25 +0100

    using useEffect in App.js, so if we store our input in local, it will still show after we refresh the page. using useEffect in Login.js, so it has a cleaner Code and make sure that the keystroke will NOT cause a call everything we onChange

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,22 +7,36 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  //if this part is not write inside useEffect, it will cause a loop, every time that state is changed!!
+  // const storedLoginInformation = localStorage.getItem('isLoggedIn');
+  // if (storedLoginInformation.value==='LoggedIn'){
+  //   setIsLoggedIn(true);
+  // }
+
+  useEffect(() => {
+    const storedLoginInformation = localStorage.getItem('isLoggedIn');
+    if (storedLoginInformation === 'LoggedIn') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', 'LoggedIn');
+    //it will store the data in the browser storage, it could be cookies or local storage
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
   };
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler}/>
       <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
+        {!isLoggedIn && <Login onLogin={loginHandler}/>}
+        {isLoggedIn && <Home onLogout={logoutHandler}/>}
       </main>
     </React.Fragment>
   );
